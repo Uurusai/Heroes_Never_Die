b66c5ff1b5178db215818b57018af1d00b8bca9f
Uurusai <138204159+Uurusai@users.noreply.github.com>
2024-10-17 16:29:08 +0600

    Add files via upload

--- global.gd
+++ global.gd
@@ -6,3 +6,10 @@ var player_body : CharacterBody2D
 var taking_damage: bool  = false 
 var crow : CharacterBody2D
 var enemy_attack : CharacterBody2D
+var bat_bod :CharacterBody2D
+var bat_damage : float = 5 
+var player_damage : float = 10
+@export var player_health:float = 30.0
+@export var player_max_health : float = 30.0
+@export var coin_max : float = 2.0
+@export var coin_count : float = 0
